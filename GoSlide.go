595b7fdc9f05913ba30477e8ac9b2500c6cd50ea
Robin Douglas <robin.douglas@trimble.com>
2018-04-26 13:51:01 +0100

    Add board types and drawing
    Add types for tiles and boards as well as functions to draw board. Also
    adds code to main to test.

---
+++ GoSlide.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+type tile = int
+
+type board = [3][3]*tile
+
+func drawBoardLine(boardLine [3]*tile) {
+	fmt.Print("|")
+	for _, t := range boardLine {
+		fmt.Print(" ")
+		fmt.Print(*t)
+		fmt.Print(" ")
+	}
+	fmt.Print("|\n")
+}
+
+func drawBoard(gameBoard board) {
+	fmt.Println("+---------+")
+	for _, l := range gameBoard {
+		drawBoardLine(l)
+	}
+	fmt.Println("+---------+")
+}
+
+func main() {
+	var tiles = [9]tile{0, 1, 2, 3, 4, 5, 6, 7, 8}
+	var testBoard = board{{&tiles[0], &tiles[1], &tiles[2]}, {&tiles[3], &tiles[4], &tiles[5]}, {&tiles[6], &tiles[7], &tiles[8]}}
+	drawBoard(testBoard)
+}
