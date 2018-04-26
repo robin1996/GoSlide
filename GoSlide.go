77355f2b57e850c8c92ac95877f627f79a03ceb9
Robin Douglas <robin.douglas@trimble.com>
2018-04-26 15:18:25 +0100

    Add blank square position vars

--- GoSlide.go
+++ GoSlide.go
@@ -14,30 +14,36 @@ const (
 	down // 3
 )
 
+var blankXPos int = 0
+var blankYPos int = 0
+var tiles = [9]tile{0, 1, 2, 3, 4, 5, 6, 7, 8}
+var gameBoard = board{{&tiles[0], &tiles[1], &tiles[2]}, 
+					  {&tiles[3], &tiles[4], &tiles[5]},
+					  {&tiles[6], &tiles[7], &tiles[8]}}
+
 type tile = int
 
 type square = *tile
 
 type board = [3][3]square
 
-func drawBoardLine(boardLine [3]square) {
-	fmt.Print("|")
-	for _, s := range boardLine {
-		fmt.Print(" ")
-		if *s == 0 {
-			fmt.Print(" ")
-		} else {
-			fmt.Print(*s)
-		}
-		fmt.Print(" ")
-	}
-	fmt.Print("|\n")
-}
-
-func drawBoard(gameBoard board) {
+// Also sets blank pos vars
+func drawBoard() {
 	fmt.Println("+---------+")
-	for _, l := range gameBoard {
-		drawBoardLine(l)
+	for y, l := range gameBoard {
+		for x, s := range l {
+			fmt.Print("|")
+			fmt.Print(" ")
+			if *s == 0 {
+				fmt.Print(" ")
+				blankYPos = y
+				blankXPos = x
+			} else {
+				fmt.Print(*s)
+			}
+			fmt.Print(" ")
+			fmt.Print("|\n")
+		}
 	}
 	fmt.Println("+---------+")
 }
@@ -47,8 +53,6 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	var tiles = [9]tile{0, 1, 2, 3, 4, 5, 6, 7, 8}
-	var testBoard = board{{&tiles[0], &tiles[1], &tiles[2]}, {&tiles[3], &tiles[4], &tiles[5]}, {&tiles[6], &tiles[7], &tiles[8]}}
 	for {
 		keyPress := termbox.PollEvent()
 		switch keyPress.Type {
@@ -72,6 +76,6 @@ func main() {
 		case termbox.EventError:
 			panic(keyPress.Err)
 			}
-			drawBoard(testBoard)
+			drawBoard()
 		}
 }
